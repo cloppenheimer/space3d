d21e8eb450fc3d609e0620d2106c74d2df487cc1
Clara <cloppenheimer@gmail.com>
2019-05-03 13:07:39 -0400

    update

--- lights.js
+++ lights.js
@@ -28,8 +28,8 @@ class Lights {
     this.ambientLight = new THREE.AmbientLight (0x404040)
 	scene.add (this.ambientLight);
 
-	console.log("updatee")
-    this.sunlight = new THREE.PointLight( 0xfffff, 1, 100 );
+	console.log("updateee")
+    this.sunlight = new THREE.PointLight( 0xffffff, 1, 100 );
 	this.sunlight.position.set( 0, 0, 0);
 	scene.add(this.sunlight );
 	}
